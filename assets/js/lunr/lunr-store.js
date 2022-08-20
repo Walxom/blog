4d9bdc943c83ecb5e5f2efed14f089ecffdc9fad
unknown <Walxom>
2022-08-21 00:11:20 +0200

    .

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Explotación de permisos SUID",
+        "excerpt":"Hay veces que es necesario que un programa se ejecute con los privilegios de su propietario en lugar de con los privilegios del usuario que lo ejecuta. Permisos SUID El bit de SUID/setuid (Set User ID) se activa sobre un fichero añadiéndole 4000 a la representación octal de los permisos...","categories": ["PrivEsc"],
+        "tags": ["linux","privesc","suid"],
+        "url": "/privesc/explotacion-de-permisos-suid/",
+        "teaser": "https://i.ibb.co/bLwX9yS/explotacion-de-permisos-suid.png"
+      },{
+        "title": "Explotación de tareas Cron",
+        "excerpt":"A menudo hay formas de hacer las cosas de manera más rápida, como por ejemplo, las tareas. Algunos webmasters en vez de hacerlo manualmente prefieren ahorrarse el tiempo y automatizarlas. Esto en parte está bien, ya que no siempre vamos a poder estar presentes para ejecutar algo, pero a veces...","categories": ["PrivEsc"],
+        "tags": ["linux","privesc","cron"],
+        "url": "/privesc/explotacion-de-tareas-cron/",
+        "teaser": "https://i.ibb.co/dPz847N/explotacion-de-tareas-cron.png"
+      },{
+        "title": "Explotación de un PATH Hijacking",
+        "excerpt":"En los sistemas operativos hay un concepto llamado las variables de entorno, quizás algunos ya hayamos usado estos sin darnos cuenta, ya sea %AppData% para encontrar el directorio donde poner los mods de Minecraft o %Temp% para borrar los archivos temporales (si eres de Microsoft). Las variables de entorno son...","categories": ["PrivEsc"],
+        "tags": ["linux","privesc","path"],
+        "url": "/privesc/explotacion-de-un-path-hijacking/",
+        "teaser": "https://i.ibb.co/kxjF9wz/path-hijacking.png"
+      },{
+        "title": "Explotación de las Capabilities",
+        "excerpt":"Anteriormente, hablamos de los permisos SUID (setuid), te hago recordar esto porque quizás esto es algo parecido, pero no. La diferencia entre estos es que si tenemos un binario con permisos SUID y ejecutas el binario, directamente asumes el rol del propietario de forma temporal. Pero si el binario tiene...","categories": ["PrivEsc"],
+        "tags": ["linux","privesc","capabilities"],
+        "url": "/privesc/explotacion-de-las-capabilities/",
+        "teaser": "https://i.ibb.co/Lt5fgcZ/explotacion-de-las-capabilities.png"
+      }]
